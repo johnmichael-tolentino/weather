0e74345fadfa544b06a3bdc5d5c606a23a9e60b7
jolentino <johnm.tolentino@gmail.com>
2021-01-02 21:12:20 -0500

    Implemented delete button

--- src/scripts/createWeatherItem.js
+++ src/scripts/createWeatherItem.js
@@ -2,30 +2,38 @@ import '../styles/createWeatherItem.css';
 
 export const createWeatherItem = ({ cityName, temp, icon, desc }) => {
 	const createElements = () => {
-		return ['div', 'h2', 'img', 'section', 'h1', 'h3'].map((elem) => {
+		return ['div', 'button', 'h2', 'img', 'section', 'h1', 'h3'].map((elem) => {
 			return document.createElement(elem);
 		});
 	};
 
 	const configureWeatherItem = () => {
-		const [div, h2, img, section, h1, h3] = createElements();
+		const [div, button, h2, img, section, h1, h3] = createElements();
 
 		// Add attributes.
 		div.id = cityName;
 		div.className = 'weather-item';
+		button.className = 'delete';
 		h2.className = 'city-name';
 		img.className = 'icon';
 		section.className = 'content';
 		h1.className = 'temperature';
 		h3.className = 'description';
 
+		// Event listener to delete weather item.
+		button.addEventListener('click', () => {
+			document.querySelector('#weather-display').removeChild(document.querySelector(`#${cityName}`));
+		});
+
 		// Insert content.
+		button.innerHTML = 'x';
 		h2.innerHTML = cityName;
 		img.src = `https://developer.accuweather.com/sites/default/files/${icon < 10 ? '0' + icon : icon}-s.png`;
 		h1.innerHTML = `${temp.Imperial.Value}°F`;
 		h3.innerHTML = desc;
 
 		// Build weather item.
+		div.appendChild(button);
 		div.appendChild(h2);
 		div.appendChild(img);
 		div.appendChild(section);
