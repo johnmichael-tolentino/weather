5096c96fe1a386558555d999a31695a1fed54bc4
jolentino <johnm.tolentino@gmail.com>
2021-01-04 22:01:45 -0500

    Secure endpoint update

--- src/scripts/fetchWeather.js
+++ src/scripts/fetchWeather.js
@@ -8,7 +8,7 @@ export const fetchWeather = async (city) => {
 	const API_KEY = '32CBtY5ZcJBGfWxjm6IJZbTgom382LhX';
 
 	// Fetch location data.
-	const response = await axios.get('http://dataservice.accuweather.com/locations/v1/cities/search', {
+	const response = await axios.get('https://dataservice.accuweather.com/locations/v1/cities/search', {
 		params: {
 			apikey: API_KEY,
 			q: city,
@@ -20,7 +20,7 @@ export const fetchWeather = async (city) => {
 	const { LocalizedName: cityName, Key } = response.data[0];
 
 	// Fetch weather data.
-	const { data } = await axios.get(`http://dataservice.accuweather.com/currentconditions/v1/${Key}`, {
+	const { data } = await axios.get(`https://dataservice.accuweather.com/currentconditions/v1/${Key}`, {
 		params: {
 			apikey: API_KEY,
 		},
